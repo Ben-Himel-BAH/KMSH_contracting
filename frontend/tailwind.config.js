8b868f931da6e2532e86560062a1e85748fd4af8
Isaac Shepard <shepard_isaac@bah.com>
2025-10-02 17:00:20 -0400

    updated App.tsx with updated layout

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",   // 👈 scan all your React/TSX files
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
